f64442791283b5d8c417849fef65e2685e961b3b
lequocchung29 <chungle924@gmail.com>
2024-07-09 01:53:33 +0700

    Create a post model and post controller

--- api/index.ts
+++ api/index.ts
@@ -5,28 +5,26 @@ import cookieParser from "cookie-parser"
 import { connectDB } from './config/database/database';
 import authRouter from './router/auth/auth.route';
 import userRouter from './router/users/user.route';
-import postRoute from './router/post/post.route';
 
-// initialize app 
+
 const app = express();
 dotenv.config();
 
-// apply middleware
+
 app.use(express.json());
 app.use(cors())
 app.use(cookieParser());
 
 // Port
-const PORT = process.env.SERVER_PORT;
+const port =  8080;
 
 //Database
 connectDB()
 
 //Router 
 app.use('/api/v1/auth/', authRouter);
-app.use('/api/v1/user/', userRouter);
-app.use('/api/v1/post', postRoute);
+app.use('/api/v1/user/', userRouter)
 
-app.listen(PORT || 8080, () => {
-    console.log(`Server running on port:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running on port:${port}`);
 })
