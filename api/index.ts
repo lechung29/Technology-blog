1c4872cdb08681806074a280a81ce5bf98058729
Killian.Le <killian.le@avepoint.com>
2024-09-24 11:30:51 +0700

    Update cors

--- api/index.ts
+++ api/index.ts
@@ -16,7 +16,9 @@ dotenv.config();
 
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: "https://dev-blog-ui.vercel.app/"
+}))
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
